9ce6548e818f86b3fd8566f9f448d3eaa077c02d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2025-04-20 03:11:41 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,10 +1,10 @@
 var mongoDB     = require('mongodb').MongoClient;
-var connection_string = 'mongodb+srv://alexbot:308boonave@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+var connection_string = 'mongodb+srv://new:newnew@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 //var connection_string = 'mongodb://127.0.0.1:27017/nodejs';
 
 if(process.env.MONGODB_PASSWORD){
-   connection_string = 'mongodb+srv://alexbot:308boonave@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+   connection_string = 'mongodb+srv://new:newnew@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
   /*
   connection_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -16,14 +16,14 @@ if(process.env.MONGODB_PASSWORD){
 }
 
 function connect(callback){
-  mongoDB.connect('mongodb+srv://alexbot:308boonave@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', function(err, db) {
+  mongoDB.connect('mongodb+srv://new:newnew@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', function(err, db) {
     if(err) throw err;
     callback(db);
   });
 }
 
 exports.getAllDocuments = function(collection, callback) {
-  mongoDB.connect('mongodb+srv://alexbot:308boonave@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', function(err, db) {
+  mongoDB.connect('mongodb+srv://new:newnew@cluster0.esmha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', function(err, db) {
     if(err) throw err;
     var allDocs = db.collection(collection).find().toArray(function(err, docs) {
       callback(docs);
